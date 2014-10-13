bdad06cb4dceb5f43179a67cf26e3453a2957116
Dave Powers <djpowers89@gmail.com>
2014-10-12 22:27:05 -0400

    Complete about triangle project 2

--- triangle.rb
+++ triangle.rb
@@ -14,6 +14,9 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
+  a, b, c = [a, b, c].sort
+  raise TriangleError if a <= 0
+  raise TriangleError if a + b <= c
   return :equilateral if a == b && a == c
   return :isosceles if a == b || a == c || b == c
   return :scalene
